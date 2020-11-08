67056e4c94c1d732d0dc97a3ee2fbf404528fa34
crass_sandwich <christopherjanidlo@gmail.com>
2020-11-08 13:21:55 -0500

    Skeleton for paddle tests

---
+++ Assets/Tests/PlayMode/PaddleTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Drepanoid.Tests.PlayMode
+{
+    public class PaddleTests
+    {
+        Paddle paddle;
+
+        [SetUp]
+        public void SetUp ()
+        {
+            paddle = Object.Instantiate(Resources.Load<Paddle>("Prefabs/Base Paddle"));
+        }
+
+        [TearDown]
+        public void TearDown ()
+        {
+            Object.Destroy(paddle);
+        }
+
+        [Test]
+        public void Paddle_ShouldMove_WhenInputIsGiven ()
+        {
+            Assert.Fail(); // TODO
+        }
+    }
+}
