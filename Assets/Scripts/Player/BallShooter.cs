b511f4859485e95de4cacbdb085d96d33743c5ae
crass_sandwich <christopherjanidlo@gmail.com>
2020-11-09 19:06:50 -0500

    Add ball shooter

---
+++ Assets/Scripts/Player/BallShooter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Drepanoid
+{
+    public class BallShooter : MonoBehaviour
+    {
+        public bool UseBallMaxSpeed;
+        public float Speed;
+
+        public Ball BallPrefab;
+
+        IEnumerator Start ()
+        {
+            yield return new WaitForSeconds(1);
+            Shoot();
+        }
+
+        public void Shoot ()
+        {
+            var ball = Instantiate(BallPrefab, transform.position, Quaternion.identity);
+            ball.Rigidbody.velocity = transform.up * (UseBallMaxSpeed ? ball.MaxSpeed : Speed);
+        }
+    }
+}
