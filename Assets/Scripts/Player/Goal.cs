a7adc57fb6990a41919df729fb0c189562710be6
crass_sandwich <christopherjanidlo@gmail.com>
2020-11-16 18:14:28 -0500

    Some weight when reaching the goal

--- Assets/Scripts/Player/Goal.cs
+++ Assets/Scripts/Player/Goal.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityAtoms.BaseAtoms;
 
 namespace Drepanoid
 {
     public class Goal : MonoBehaviour
     {
+        public AnimationCurve TimeScaleByDistanceFromCollider;
+        public float Delay;
+
+        public Vector2Variable BallPosition;
         public UnityEvent BallReachedGoal;
+
+        public BoxCollider2D Collider;
+
+        IEnumerator victoryEnum;
+
+        void Update ()
+        {
+            var dist = Vector2.Distance(transform.position, BallPosition.Value);
+            Time.timeScale = TimeScaleByDistanceFromCollider.Evaluate(dist);
+        }
 
         void OnTriggerEnter2D (Collider2D collision)
         {
-            if (collision.GetComponent<Ball>() != null)
-            {
-                BallReachedGoal.Invoke();
-            }
+            if (collision.GetComponent<Ball>() != null && victoryEnum == null)
+                StartCoroutine(victoryEnum = victoryRoutine());
+        }
+
+        IEnumerator victoryRoutine ()
+        {
+            yield return new WaitForSecondsRealtime(Delay);
+            BallReachedGoal.Invoke();
+
+            victoryEnum = null;
         }
     }
 }
